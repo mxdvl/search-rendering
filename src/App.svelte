c92bf8baf6043581f341e667805b4ef75bfe719c
Max Duval <max.duval@guardian.co.uk>
2021-06-28 10:26:44 +0100

    better raw results log

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script>
   import { timeAgo } from "@guardian/libs";
-  import { data } from './data.js';
-  import { text, news, sport, culture, brand, opinion, lifestyle } from "@guardian/src-foundations/palette"
-  import { body , headline } from "@guardian/src-foundations/typography"
+  import { data } from "./data.js";
+  import {
+    text,
+    news,
+    sport,
+    culture,
+    opinion,
+    lifestyle,
+  } from "@guardian/src-foundations/palette";
+  import { body, headline } from "@guardian/src-foundations/typography";
 
   let search;
   let results = data;
@@ -14,17 +21,17 @@
     fetch(`${ENDPOINT}?q=${search}&api-key=test`)
       .then((raw) => raw.json())
       .then((response) => {
-        console.log("raw results", response);
+        console.log("raw results", response.response.result);
 
-		console.log("raw results", JSON.stringify(response.response.results));
+        // console.log("raw results", JSON.stringify(response.response.results));
         results = response.response.results;
       });
   };
 
   const simplifyPillar = (pillarId) => {
-    const [,cleanId] = pillarId.split('/');
-    return cleanId.replace(/(arts|books)/, 'culture');
-  }
+    const [, cleanId] = (pillarId ?? "/news").split("/");
+    return cleanId.replace(/(arts|books)/, "culture");
+  };
 
   const style = `
     --primary: ${text.primary};
@@ -34,7 +41,7 @@
     --culture: ${culture[400]};
     --opinion: ${opinion[400]};
     --lifestyle: ${lifestyle[400]};
-    `.replaceAll(/\s+/g,' ');
+    `.replaceAll(/\s+/g, " ");
 </script>
 
 <main style={body.medium() + style}>
@@ -44,21 +51,24 @@
     <input bind:value={search} type="text" placeholder="coronavirus" />
     <button type="submit" disabled={!search}>Search</button>
   </form>
-  
+
   <ul>
     {#each results as result}
-    <li>
-      <a class="result" target="_blank" href={result.webUrl}>
-        <h3 style={headline.xsmall()}>
-          <span style="color: var(--{simplifyPillar(result.pillarId)})">{result.sectionName} /</span>
-          {result.webTitle}
-        </h3>
-        <h5 style={body.small()} >{timeAgo(new Date(result.webPublicationDate).getTime())}</h5>
-      </a>
-    </li>
+      <li>
+        <a class="result" target="_blank" href={result.webUrl}>
+          <h3 style={headline.xsmall()}>
+            <span style="color: var(--{simplifyPillar(result.pillarId)})"
+              >{result.sectionName} /</span
+            >
+            {result.webTitle}
+          </h3>
+          <h5 style={body.small()}>
+            {timeAgo(new Date(result.webPublicationDate).getTime())}
+          </h5>
+        </a>
+      </li>
     {/each}
   </ul>
-  
 </main>
 
 <style>
@@ -82,17 +92,17 @@
   }
 
   .result {
-	  display: flex;
-	  flex-wrap: wrap;
-	  margin-bottom: 1em;
-	  color: var(--primary);
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 1em;
+    color: var(--primary);
   }
 
   h3 {
     line-height: 1.15;
-	  font-weight: 500;
-	  width: 100%;
-	  margin: 0;
+    font-weight: 500;
+    width: 100%;
+    margin: 0;
   }
 
   a {
@@ -103,15 +113,16 @@
     text-decoration: underline;
   }
 
-  h4, h5 {
-	  font-weight: 400;
-	  font-size: 1em;
-	  margin: 0;
-	  text-decoration: none;
+  h4,
+  h5 {
+    font-weight: 400;
+    font-size: 1em;
+    margin: 0;
+    text-decoration: none;
   }
 
   h4 {
-	  width: 6em;
+    width: 6em;
   }
 
   @media (min-width: 640px) {
