8b0a6bf4b10ed2c1889ff0cf43dddfd1d6904607
Max Duval <max.duval@theguardian.com>
2022-09-30 17:22:56 +0100

    fix: remove legacy fonts

--- src/fonts.ts
+++ src/fonts.ts
@@ -72,104 +72,62 @@ export const fonts = [
     weight: 700,
     style: "italic",
   },
-  // GuardianTextEgyptian, with legacy family name of Guardian Text Egyptian Web
   {
     family: "GuardianTextEgyptian",
     file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-Regular",
     weight: 400,
     style: "normal",
   },
-  {
-    family: "Guardian Text Egyptian Web",
-    file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-Regular",
-    weight: 400,
-    style: "normal",
-  },
+
   {
     family: "GuardianTextEgyptian",
     file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-RegularItalic",
     weight: 400,
     style: "italic",
   },
-  {
-    family: "Guardian Text Egyptian Web",
-    file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-RegularItalic",
-    weight: 400,
-    style: "italic",
-  },
+
   {
     family: "GuardianTextEgyptian",
     file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-Bold",
     weight: 700,
     style: "normal",
   },
-  {
-    family: "Guardian Text Egyptian Web",
-    file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-Bold",
-    weight: 700,
-    style: "normal",
-  },
+
   {
     family: "GuardianTextEgyptian",
     file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-BoldItalic",
     weight: 700,
     style: "italic",
   },
-  {
-    family: "Guardian Text Egyptian Web",
-    file: "guardian-textegyptian/noalts-autohinted/GuardianTextEgyptian-BoldItalic",
-    weight: 700,
-    style: "italic",
-  },
-  // GuardianTextSans, with legacy family name of Guardian Text Sans Web
+
   {
     family: "GuardianTextSans",
     file: "guardian-textsans/noalts-autohinted/GuardianTextSans-Regular",
     weight: 400,
     style: "normal",
   },
-  {
-    family: "Guardian Text Sans Web",
-    file: "guardian-textsans/noalts-autohinted/GuardianTextSans-Regular",
-    weight: 400,
-    style: "normal",
-  },
+
   {
     family: "GuardianTextSans",
     file: "guardian-textsans/noalts-autohinted/GuardianTextSans-RegularItalic",
     weight: 400,
     style: "italic",
   },
-  {
-    family: "Guardian Text Sans Web",
-    file: "guardian-textsans/noalts-autohinted/GuardianTextSans-RegularItalic",
-    weight: 400,
-    style: "italic",
-  },
+
   {
     family: "GuardianTextSans",
     file: "guardian-textsans/noalts-autohinted/GuardianTextSans-Bold",
     weight: 700,
     style: "normal",
   },
-  {
-    family: "Guardian Text Sans Web",
-    file: "guardian-textsans/noalts-autohinted/GuardianTextSans-Bold",
-    weight: 700,
-    style: "normal",
-  },
+
   {
     family: "GuardianTextSans",
     file: "guardian-textsans/noalts-autohinted/GuardianTextSans-BoldItalic",
     weight: 700,
     style: "italic",
   },
-  {
-    family: "Guardian Text Sans Web",
-    file: "guardian-textsans/noalts-autohinted/GuardianTextSans-BoldItalic",
-    weight: 700,
-    style: "italic",
-  },
+
   {
     family: "GT Guardian Titlepiece",
     file: "guardian-titlepiece/noalts-autohinted/GTGuardianTitlepiece-Bold",
